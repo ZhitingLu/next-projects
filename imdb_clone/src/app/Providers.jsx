a462209adf27667fded80517b8d7ef19b7cd6106
Zhiting Lu <zhitinglu@outlook.com>
2025-04-26 20:27:59 +0200

    added Providers component

---
+++ imdb_clone/src/app/Providers.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+import React from "react";
+
+export default function Providers({ children }) {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="min-h-screen select-none transition-color duration-300">{children}</div>
+    </ThemeProvider>
+  );
+}
