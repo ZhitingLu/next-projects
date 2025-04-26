bceb34af61d36de414f326ac7408784834705e98
Zhiting Lu <zhitinglu@outlook.com>
2025-04-26 20:59:35 +0200

    added About page

---
+++ imdb_clone/src/app/about/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDb Clone, your ultimate destination for movie and TV show
+        information! Our platform offers an extensive database of films, TV
+        shows, actors, directors, and more, bringing you the latest and greatest
+        in entertainment.
+      </p>
+      <p>
+        Whether you're a movie buff looking to explore new releases, a fan
+        seeking information about your favorite stars, or someone in need of
+        inspiration for your next binge-watch, IMDb Clone has got you covered.
+        We strive to provide the most up-to-date and accurate data to enhance
+        your entertainment experience.
+      </p>
+      <p>
+        Our mission is simple: to connect people with the movies and TV shows
+        they love and make discovering new content as fun and easy as possible.
+        Join our community and explore a world of entertainment!
+      </p>
+    </div>
+  );
+}
