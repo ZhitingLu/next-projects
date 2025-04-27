e233347245e6914ca8529524673dfc8defc9e850
Zhiting Lu <zhitinglu@outlook.com>
2025-04-27 15:56:44 +0200

    created NavBar component

---
+++ imdb_clone/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBarItem from './NavBarItem'
+
+export default function NavBar() {
+  return (
+    <div className='flex gap-6 bg-amber-100 dark:bg-gray-600 
+    p-4 lg:text-lg justify-center'>
+        <NavBarItem title="Trending" param="fetchTrending" />
+        <NavBarItem title="Top Rated" param="fetchTopRated" />
+
+    </div>
+  )
+}
