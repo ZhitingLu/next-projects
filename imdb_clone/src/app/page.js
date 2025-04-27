aa297019d958c45eac82a42e9c8dd0dfd0b6d35d
Zhiting Lu <zhitinglu@outlook.com>
2025-04-27 17:43:01 +0200

    added loading effect using next.js

--- imdb_clone/src/app/page.js
+++ imdb_clone/src/app/page.js
@@ -10,12 +10,18 @@ export default async function page({ searchParams }) {
   // Use a fallback value in case 'genre' is undefined
   const genreValue = genre || "fetchTrending";
 
+  // Introduce a 2-second delay before fetching
+  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+  // Wait for 2 seconds before continuing with fetch
+  await delay(1000);
+
   const apiUrl = `https://api.themoviedb.org/3/${
     genreValue === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
   }?api_key=${API_KEY}&
     language=en-US&page=1`;
-  
-  const res = await fetch(apiUrl);
+
+  const res = await fetch(apiUrl, { next: { revalidate: 1000 } });
 
   const data = await res.json();
   if (!res.ok) {
@@ -23,9 +29,10 @@ export default async function page({ searchParams }) {
   }
 
   const results = data.results;
-  
 
-  return <div>
-    <Results results={results} />
-  </div>;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
