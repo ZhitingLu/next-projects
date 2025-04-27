a4521636e25deaa33ba07f2a9590647d0536a5d3
Zhiting Lu <zhitinglu@outlook.com>
2025-04-27 15:57:18 +0200

    updated style sheet

---
+++ imdb_clone/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    darkMode: 'class', // ⚡ Tell Tailwind: dark mode is toggled by a class!
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
