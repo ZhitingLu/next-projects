a1a42e4f9c28f3291c76bc932ca28f27f32799d5
Zhiting Lu <zhitinglu@outlook.com>
2025-05-01 17:42:33 +0200

    wrapped useSearchParams with Suspense

--- imdb_clone/src/components/NavBar.jsx
+++ imdb_clone/src/components/NavBar.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import NavBarItem from './NavBarItem'
+import React, { Suspense } from "react";
+import NavBarItem from "./NavBarItem";
 
 export default function NavBar() {
   return (
-    <div className='flex gap-6 bg-amber-100 dark:bg-gray-700 
-    p-4 lg:text-lg justify-center'>
+    <div
+      className="flex gap-6 bg-amber-100 dark:bg-gray-700 
+    p-4 lg:text-lg justify-center"
+    >
+      <Suspense fallback={<div>Loading...</div>}>
         <NavBarItem title="Trending" param="fetchTrending" />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <NavBarItem title="Top Rated" param="fetchTopRated" />
-
+      </Suspense>
     </div>
-  )
+  );
 }
