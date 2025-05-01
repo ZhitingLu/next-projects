27a1c358d1673e7be4189d87d70e28529937a630
Zhiting Lu <zhitinglu@outlook.com>
2025-05-01 17:41:43 +0200

    Await params before accessing its properties

--- imdb_clone/src/app/search/[searchTerm]/page.jsx
+++ imdb_clone/src/app/search/[searchTerm]/page.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Results from "@/components/Results";
 
 export default async function SearchPage({ params }) {
-  const searchTerm = params.searchTerm;
+  // Await params before accessing its properties
+  const {searchTerm} = await params;
+  
   const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`;
   const res = await fetch(apiUrl);
   const data = await res.json();
@@ -10,9 +12,11 @@ export default async function SearchPage({ params }) {
 
   return (
     <div>
-      {results && results.length ===
-        (<h1 className="text-center mt-10">No results found</h1>)}
-      {results && <Results results={results} />}
+      {results && results.length === 0 ?
+        (<h1 className="text-center mt-10">No results found</h1>) 
+      : (
+      results && <Results results={results} />
+    )}
     </div>
   );
 }
