7f8f5b7b37f7c3d38633adcd6a64658ad2c00efe
Zhiting Lu <zhitinglu@outlook.com>
2025-05-05 22:11:27 +0200

    fixed styles

--- google-next/src/components/HomeHeader.jsx
+++ google-next/src/components/HomeHeader.jsx
@@ -5,7 +5,7 @@ import { TbGridDots } from "react-icons/tb";
 export default function HomeHeader() {
   return (
     <header className="flex justify-end p-5 text-sm">
-      <div className="flex space-x-4 item-center">
+      <div className="flex space-x-4 items-center">
         <Link href={"https://mail.google.com"} className="hover:underline">
           Gmail
         </Link>
