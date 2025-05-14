d4a386e4d96eae63e996a9195b977e921876982b
Zhiting Lu <zhitinglu@outlook.com>
2025-05-14 22:28:56 +0200

    Fetching data from google search API

--- google-next/src/app/search/web/page.jsx
+++ google-next/src/app/search/web/page.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+
   return (
-    <div>WebSearchPage</div>
-  )
+    <div>
+      {results &&
+        results.map((result, index) => <h1 key={index}>{result.title}</h1>)}
+    </div>
+  );
 }
