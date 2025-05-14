bf6b04d17d348237633ba47b0ec30422858c41f3
Zhiting Lu <zhitinglu@outlook.com>
2025-05-15 00:32:13 +0200

    handling errors

--- google-next/src/app/search/web/page.jsx
+++ google-next/src/app/search/web/page.jsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({searchParams}) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+  if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
   const results = data.items;
+
+  if(!results) {
+    return (
+        <div className="flex flex-col justify-center items-center pt-10">
+            <h1 className="text-2xl mb-4">
+            No results found for <span className="italic text-gray-600">'{searchParams.searchTerm}'</span>
+            </h1>
+            <p className="text-lg">Try searching for something else</p>
+            <Link href='/' className="text-blue-500 mt-2">Home</Link>
+        </div>
+    );
+  }
 
   return (
     <div>
