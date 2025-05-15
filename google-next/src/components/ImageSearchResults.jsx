3b3df811a4edc802b78bd8595aba8f58dc705369
Zhiting Lu <zhitinglu@outlook.com>
2025-05-15 07:27:34 +0200

    updated prop

--- google-next/src/components/ImageSearchResults.jsx
+++ google-next/src/components/ImageSearchResults.jsx
@@ -8,7 +8,7 @@ export default function ImageSearchResults({ results }) {
   return (
     <div className="sm:pb-24 pb-40 mt-4 ">
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 xl:grid-cols-8 px-3 space-x-4">
-        {results.items.map((result) => (
+        {results.map((result) => (
           <div className="mb-8" key={result.link}>
             <div className="group">
               <Link href={result.image.contextLink}>
