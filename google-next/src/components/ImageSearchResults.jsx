8ef5d62e3c320b0376d1b2c9cc13cbe32b0a1696
Zhiting Lu <zhitinglu@outlook.com>
2025-05-15 02:41:29 +0200

    included pagination

--- google-next/src/components/ImageSearchResults.jsx
+++ google-next/src/components/ImageSearchResults.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
 export default function ImageSearchResults({ results }) {
   return (
-    <div className="pb-24 mt-4 ">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 px-3 space-x-4">
+    <div className="sm:pb-24 pb-40 mt-4 ">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 xl:grid-cols-8 px-3 space-x-4">
         {results.items.map((result) => (
           <div className="mb-8" key={result.link}>
             <div className="group">
@@ -28,6 +29,9 @@ export default function ImageSearchResults({ results }) {
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+        <PaginationButtons />
       </div>
     </div>
   );
