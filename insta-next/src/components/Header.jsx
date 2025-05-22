dc07e16565c203256ea2feaaa37cdc69e416c1f2
ZhitingLu <zhitinglu@outlook.com>
2025-05-22 17:37:48 +0200

    added modal

--- insta-next/src/components/Header.jsx
+++ insta-next/src/components/Header.jsx
@@ -1,13 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Modal from "react-modal";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
 
 export default function Header() {
   const { data: session } = useSession();
-  console.log(session);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="shadow-sm border-b-gray sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -27,21 +31,52 @@ export default function Header() {
           rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* menu items */}
-        
+
         {
           session ? (
-            <img src={session.user.image} alt={session.user.name} 
-            className="h-10 w-10 rounded-full cursor-pointer"
-            onClick={signOut}
-             />
+            <div className="flex gap-2 items-center">
+              <IoMdAddCircleOutline className="text-2xl cursor-pointer transform hover:scale-125
+              transition duration-300 hover:text-red-600"
+                onClick={() => setIsOpen(true)}
+              />
+              <img src={session.user.image} alt={session.user.name}
+                className="h-10 w-10 rounded-full cursor-pointer"
+                onClick={signOut}
+              />
+            </div>
           )
-          :
-        (<button
-          onClick={() => signIn()}
-          className="text-sm font-semibold text-blue-500 cursor-pointer"
-        >Log In</button>)
+            :
+            (<button
+              onClick={() => signIn()}
+              className="text-sm font-semibold text-blue-500 cursor-pointer"
+            >Log In</button>)
         }
       </div>
+      {
+        isOpen && (
+          <Modal isOpen={isOpen}
+            className="max-w-lg w-[90%] p-6 absolute top-56 left-1/2 -translate-x-1/2  bg-white 
+            border-2 rounded-md shadow-md"
+            onRequestClose={() => setIsOpen(false)}
+            ariaHideApp={false}
+          >
+            <div className="flex flex-col justify-center items-center h-full">
+              <HiCamera className="text-5xl text-gray-400 cursor-pointer" />
+             
+
+            </div>
+
+            <input type="text" maxLength="150"
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+            placeholder="Please enter your caption..."
+            />
+             <button
+                className="w-full bg-red-600 text-white p-2 rounded-lg shadow-md hover:brightness-105 disabled:bg-gray-200 
+                disabled:hover:brightness-100"
+              >Upload Post</button>
+          </Modal>
+        )
+      }
     </div>
   );
 }
