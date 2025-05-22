3f557ed769eef149161476e5bdb246b6617cea06
ZhitingLu <zhitinglu@outlook.com>
2025-05-22 17:00:28 +0200

    customized signIn and Logout on the Header

--- insta-next/src/components/Header.jsx
+++ insta-next/src/components/Header.jsx
@@ -3,9 +3,11 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b-gray sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -25,10 +27,20 @@ export default function Header() {
           rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* menu items */}
-        <button
+        
+        {
+          session ? (
+            <img src={session.user.image} alt={session.user.name} 
+            className="h-10 w-10 rounded-full cursor-pointer"
+            onClick={signOut}
+             />
+          )
+          :
+        (<button
           onClick={() => signIn()}
-          className="text-sm font-semibold text-blue-500"
-        >Log In</button>
+          className="text-sm font-semibold text-blue-500 cursor-pointer"
+        >Log In</button>)
+        }
       </div>
     </div>
   );
