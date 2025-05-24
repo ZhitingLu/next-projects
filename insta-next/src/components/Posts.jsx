a6e22d47b75f66ed665d729aaaeb59b5188ebab2
Zhiting Lu <zhitinglu@outlook.com>
2025-05-24 19:51:48 +0200

    completed Posts cards

--- insta-next/src/components/Posts.jsx
+++ insta-next/src/components/Posts.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { app } from "../firebase";
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+import Post from "./Post";
 
-export default function Posts() {
+export default async function Posts() {
+  // This component will fetch and display posts.
+  const db = getFirestore(app);
+  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+  const querySnapshot = await getDocs(q);
+  let posts = [];
+  querySnapshot.forEach((doc) => {
+    posts.push({ id: doc.id, ...doc.data() });
+  });
+
   return (
-    <div>Posts</div>
-  )
+    <div>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
