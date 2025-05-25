72dd756a65981280dc548d0a8f6928011664e109
Zhiting Lu <zhitinglu@outlook.com>
2025-05-25 16:33:00 +0200

    added like funcitonality to the post card

--- insta-next/src/components/LikeSection.jsx
+++ insta-next/src/components/LikeSection.jsx
@@ -3,7 +3,14 @@
 import { React, useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { HiOutlineHeart, HiHeart } from "react-icons/hi";
-import { collection, getFirestore, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  doc,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import { app } from "@/firebase";
 
 export default function LikeSection({ id }) {
@@ -40,9 +47,42 @@ export default function LikeSection({ id }) {
     }
   }, [likes]);
 
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session?.user?.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session?.user?.uid), {
+        username: session?.user?.username,
+      });
+    }
+  }
+
   return (
     <div>
-      {session && <div className="flex border-t border-gray-100"></div>}
+      {session && (
+        <div className="flex border-t border-gray-100 px-4 pt-4">
+          <div className="flex items-center gap-2">
+            {hasLiked ? (
+              <HiHeart
+                onClick={likePost}
+                className="text-red-500 cursor-pointer
+              text-3xl hover:scale-125 transtition-transform duration-200 ease-out"
+              />
+            ) : (
+              <HiOutlineHeart
+                onClick={likePost}
+                className="cursor-pointer
+              text-3xl hover:scale-125 transtition-transform duration-200 ease-out"
+              />
+            )}
+            {likes.length > 0 && (
+              <p className="text-gray-500">
+                {likes.length} {likes.length === 1 ? "like" : "likes"}
+              </p>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
