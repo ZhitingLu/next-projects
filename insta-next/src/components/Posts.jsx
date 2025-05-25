e63279a945e4a5e6daad6cede38d105a2c1c84b8
Zhiting Lu <zhitinglu@outlook.com>
2025-05-25 18:08:45 +0200

    fix real-time update: use onSnapshot instead of getDocs

--- insta-next/src/components/Posts.jsx
+++ insta-next/src/components/Posts.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import { app } from "../firebase";
 import {
   getFirestore,
   collection,
   query,
   orderBy,
-  getDocs,
+  onSnapshot, // Use onSnapshot for real-time updates
 } from "firebase/firestore";
 import Post from "./Post";
 
-export default async function Posts() {
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
   // This component will fetch and display posts.
   const db = getFirestore(app);
-  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
-  const querySnapshot = await getDocs(q);
-  let posts = [];
-  querySnapshot.forEach((doc) => {
-    posts.push({ id: doc.id, ...doc.data() });
-  });
+  // const querySnapshot = await getDocs(q); // getDocs() is a one-time fetch
+  // Use onSnapshot instead of getDocs
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+
+    // real-tiem listener
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const updatedPosts = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setPosts(updatedPosts);
+    });
+
+    return () => {
+      // Cleanup the listener on unmount
+      unsubscribe();
+    };
+  }, [db]);
 
   return (
     <div>
