749e96d1923f27d245dce970ed7b013bd9957929
Zhiting Lu <zhitinglu@outlook.com>
2025-05-25 18:53:22 +0200

    included Toaster to the app

--- insta-next/src/app/layout.js
+++ insta-next/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import SessionWrapper from "@/components/SessionWrapper";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
           <Header />
 
           {children}
+          <Toaster position="top-center" />
         </body>
       </html>
     </SessionWrapper>
