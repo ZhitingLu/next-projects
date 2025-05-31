0e47980a5c3603feaeb50bcec2aa531f446a65db
Zhiting Lu <zhitinglu@outlook.com>
2025-05-31 20:31:07 +0200

    style: update News component layout and button styling

--- x-next/src/app/components/News.jsx
+++ x-next/src/app/components/News.jsx
@@ -16,14 +16,14 @@ export default function News() {
   }, []);
 
   return (
-    <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 pb-5">
+    <div className="border border-gray-100 text-slate-900 space-y-3 bg-white rounded-xl pt-2 pb-5">
       <h4 className="font-bold text-xl px-4">What's happening</h4>
       {news.slice(0, articleNum).map((article, index) => (
         <div key={index} className="">
           <a href={article.url} target="_blank" className="">
             <div
               className="flex items-center justify-between px-4 py-2 
-            space-x-1 hover:bg-gray-200 transition duration-200"
+            space-x-1 hover:bg-gray-100 transition duration-200"
             >
               <div className="space-y-0.5">
                 <h6 className="text-sm font-bold">{article.title}</h6>
@@ -43,10 +43,17 @@ export default function News() {
           </a>
         </div>
       ))}
-      <button
-      onClick={() => {setArticleNum(articleNum + 3)}}
-      className="text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm cursor-pointer
-      " >Load more</button>
+      <div className="hover:bg-gray-100 py-3 cursor-pointer">
+        <button
+          onClick={() => {
+            setArticleNum(articleNum + 3);
+          }}
+          className="text-[var(--twitter-blue)] pl-4 font-semibold hover:text-blue-400 text-sm cursor-pointer 
+      "
+        >
+          Load more
+        </button>
+      </div>
     </div>
   );
 }
