0c280e0121ff1a89fa493f929f42804ebbee9192
Zhiting Lu <zhitinglu@outlook.com>
2025-06-01 18:43:18 +0200

    feat: add custom session properties for user authentication

--- x-next/src/app/api/auth/[...nextauth]/route.js
+++ x-next/src/app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,19 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
+    // This callback is called whenever a user signs in
+    async session({ session, token }) {
+      // Add custom properties to the session object
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub; // Use the sub from the token as the uid
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
