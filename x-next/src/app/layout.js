6df5619cae100239549aa6d5820fef38317fa34e
Zhiting Lu <zhitinglu@outlook.com>
2025-06-01 16:48:23 +0200

    integrate SearchBar and adjust layout for improved component organization

--- x-next/src/app/layout.js
+++ x-next/src/app/layout.js
@@ -1,11 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Sidebar from "./components/Sidebar";
-import News from "./components/News";
-import SessionWrapper from "./components/SessionWrapper";
-import { FiSearch } from "react-icons/fi";
-import Subscription from "./components/Subscription";
 import FollowSuggestions from "./components/FollowSuggestions";
+import News from "./components/News";
+import SearchBar from "./components/SearchBar";
+import SessionWrapper from "./components/SessionWrapper";
+import Sidebar from "./components/Sidebar";
+import Subscription from "./components/Subscription";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,29 +31,20 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <div className="flex justify-between max-w-7xl mx-auto min-h-screen">
-            <div className="hidden sm:flex flex-col border-r border-gray-200 h-screen pr-4">
+            <div className="hidden sm:flex w-[280px] flex-col border-r border-gray-200 h-screen">
               <Sidebar />
             </div>
 
-            <div className="flex-grow min-w-0 max-w-[600px] mx-auto">
+            <div className="flex-grow min-w-0 max-w-[600px] mx-auto border-r border-gray-100">
               {children}
             </div>
 
-            <aside className="hidden lg:flex flex-col w-80 border-l border-gray-200">
-              <div className="sticky top-0 bg-white py-2 ">
-                <FiSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600 w-4 h-4" />
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="border border-gray-200 rounded-3xl bg-white
-              text-sm w-full px-4 py-2.5 pl-10 placeholder-gray-700
-               placeholder:font-semibold focus:outline-none focus:border-blue-400 focus:ring-1 focus:ring-blue-400"
-                ></input>
-              </div>
+            <div className="hidden lg:flex flex-col gap-3 w-[22rem] h-screen ">
+              <SearchBar />
               <Subscription />
               <FollowSuggestions />
               <News />
-            </aside>
+            </div>
           </div>
         </body>
       </html>
