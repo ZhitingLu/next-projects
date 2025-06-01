5b65bde4ab31abf504165ad162e998a49a9fca45
Zhiting Lu <zhitinglu@outlook.com>
2025-06-01 16:49:27 +0200

    style: update text color and size in Subscription component for better readability

--- x-next/src/app/components/Subscription.jsx
+++ x-next/src/app/components/Subscription.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Subscription() {
   return (
-    <div className="border border-gray-200 rounded-xl p-3 my-3">
+    <div className="border border-gray-200 rounded-xl p-3">
       <div className="flex flex-col gap-2">
         <h2 className="text-xl font-bold">Subscribe to Premium</h2>
-        <p className="text-gray-600">
+        <p className="text-gray-800 text-sm">
           Subscribe to unlock new features and if eligible, receive a share of
           revenue.
         </p>
