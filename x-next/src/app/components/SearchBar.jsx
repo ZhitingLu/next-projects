c52393d1755dd3c9ea672be9c6d8e58e146e8c3c
Zhiting Lu <zhitinglu@outlook.com>
2025-06-01 16:49:09 +0200

    feat: add SearchBar component for user search functionality

---
+++ x-next/src/app/components/SearchBar.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+
+export default function SearchBar() {
+  const [focused, setFocused] = useState(false);
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="relative pt-2">
+      <div className="sticky top-0 bg-white">
+        <FiSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600 w-4 h-4" />
+        <input
+          type="text"
+          placeholder="Search"
+          onFocus={() => setFocused(true)}
+          onBlur={() => query === "" && setFocused(false)}
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          className="border border-gray-200 rounded-3xl bg-white
+              text-sm w-full px-4 py-2.5 pl-10 placeholder-gray-700 z-10
+               placeholder:font-semibold focus:outline-none focus:border-blue-400 focus:ring-1 focus:ring-blue-400"
+        />
+
+        {/* Search results */}
+        {focused && query === "" && (
+          <div
+            className="absolute top-full left-0 w-full bg-white border rounded-xl
+             border-gray-100 shadow-lg p-4 translate-y-0.25"
+          >
+            <p className="text-gray-500 text-sm font-semibold">
+              {" "}
+              Try searching for people, lists, or keywords
+            </p>
+          </div>
+        )}
+
+        {query !== "" && (
+            <div
+                className="absolute top-full left-0 w-full bg-white border
+                 border-gray-100 rounded-xl shadow-lg p-4 translate-y-0.25"
+            >
+                <p className="text-gray-500">Showing results for "{query}"</p>
+                {/* logic to display search results */}
+            </div>
+        )}
+      </div>
+    </div>
+  );
+}
