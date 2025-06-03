82e3b91a21424702f0054aa9aef3d562ebb41bac
Zhiting Lu <zhitinglu@outlook.com>
2025-06-03 18:06:55 +0200

    implement image upload functionality with Firebase storage

--- x-next/src/app/components/Input.jsx
+++ x-next/src/app/components/Input.jsx
@@ -5,9 +5,63 @@ import { HiOutlineEmojiHappy, HiOutlinePhotograph } from "react-icons/hi";
 import { MdOutlineGifBox } from "react-icons/md";
 import { TbCalendarClock } from "react-icons/tb";
 import { IoLocationOutline } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/firebase";
 
 export default function Input() {
   const { data: session } = useSession();
+  const imagePickRef = useRef(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [selectedImageFile, setSelectedImageFile] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+
+  const handleImagePick = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setSelectedImageFile(file);
+    setImageFileUrl(URL.createObjectURL(file));
+  };
+
+  useEffect(() => {
+    if (selectedImageFile) {
+      uploadImageToStorage();
+    }
+  }, [selectedImageFile]);
+
+  const uploadImageToStorage = async () => {
+    setImageFileUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + selectedImageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedImageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        console.error("Error uploading image:", error);
+        setImageFileUploading(false);
+        setImageFileUrl(null);
+        setSelectedImageFile(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
+
   if (!session) return null;
 
   return (
@@ -23,9 +77,27 @@ export default function Input() {
           row="3"
           className="w-full border-b border-gray-100 outline-none tracking-wide min-h-[50px] text-gray-700 placeholder:text-gray-600 placeholder:text-xl"
         ></textarea>
+        {selectedImageFile && (
+          <img
+            src={imageFileUrl}
+            alt="Selected"
+            className="mt-2 max-h-60 cursor-pointer object-cover"
+          />
+        )}
         <div className="flex items-center justify-between pt-1.5">
           <div className="flex">
-            <HiOutlinePhotograph className="h-9 w-9 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
+            <HiOutlinePhotograph
+              className="h-9 w-9 p-2 text-sky-500 hover:bg-sky-100 rounded-full 
+            cursor-pointer"
+              onClick={() => imagePickRef.current.click()}
+            />
+            <input
+              type="file"
+              className="hidden"
+              ref={imagePickRef}
+              accept="image/*"
+              onChange={handleImagePick}
+            />
             <MdOutlineGifBox className="h-9 w-9 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
             <HiOutlineEmojiHappy className="h-9 w-9 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
             <TbCalendarClock className="h-9 w-9 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
