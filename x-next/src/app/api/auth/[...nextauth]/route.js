ab199ed7638959f1568466c7f1abd3afb3cdfebc
Zhiting Lu <zhitinglu@outlook.com>
2025-06-03 21:54:36 +0200

    refactor session callback to handle username assignment

--- x-next/src/app/api/auth/[...nextauth]/route.js
+++ x-next/src/app/api/auth/[...nextauth]/route.js
@@ -12,12 +12,16 @@ const handler = NextAuth({
   callbacks: {
     // This callback is called whenever a user signs in
     async session({ session, token }) {
-      // Add custom properties to the session object
-      session.user.username = session.user.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
-      session.user.uid = token.sub; // Use the sub from the token as the uid
+      if (session.user.name && typeof session.user.name === "string") {
+        session.user.username = session.user.name
+          .trim()
+          .split(/\s+/) // split on one or more spaces (handles multiple spaces)
+          .join("")
+          .toLowerCase();
+      } else {
+        session.user.username = "unknown";
+      }
+      session.user.uid = token.sub;
       return session;
     },
   },
