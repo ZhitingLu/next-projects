24a9c178d0c27dee128fd446cac1847810cf6754
Zhiting Lu <zhitinglu@outlook.com>
2025-06-04 21:53:40 +0200

    add Icons component to Post

--- x-next/src/app/components/Post.jsx
+++ x-next/src/app/components/Post.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { HiDotsHorizontal } from "react-icons/hi";
 import Link from "next/link";
+import Icons from "./Icons";
 
 export default function Post({ post, id }) {
   return (
-    <div className="flex p-3 border-b border-gray-200">
+    <div className="w-full flex p-3 border-b border-gray-200 hover:bg-gray-50">
       <img
         src={post?.profileImg}
         alt=""
-        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95 transition-all duration-200 mr-4"
+        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95 transition-all duration-200 mr-4
+       "
       />
       <div className="flex-1">
         <div className="flex items-center justify-between">
@@ -26,7 +28,9 @@ export default function Post({ post, id }) {
         <Link href={`/posts/${id}`}>
           <img src={post?.image} alt="" className="rounded-2xl mr-2" />
         </Link>
+        <Icons />
       </div>
+
     </div>
   );
 }
