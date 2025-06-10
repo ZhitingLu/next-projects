e4cb0b3883181b05a9a6a15363d1fe624dff4382
Zhiting Lu <zhitinglu@outlook.com>
2025-06-10 20:37:33 +0200

    support additional modal data and improve state management

--- x-next/src/app/stores/modalStore.js
+++ x-next/src/app/stores/modalStore.js
@@ -3,12 +3,16 @@ import { create } from "zustand";
 const useModalStore = create((set) => ({
   isOpen: false,
   modalType: null, // 'post', 'auth' or 'comment
-  openModal: (type) => set({ isOpen: true, modalType: type }),
-  closeModal: () => set({ isOpen: false, modalType: null }),
+  modalData: null, // Additional data for the modal if needed
+
+  openModal: (type, data = null) =>
+    set({ isOpen: true, modalType: type, modalData: data }),
+  closeModal: () => set({ isOpen: false, modalType: null, modalData: null }),
   toggleModal: () =>
     set((state) => ({
       isOpen: !state.isOpen,
       modalType: state.isOpen ? null : state.modalType,
+      modalData: state.isOpen ? null : state.modalData,
     })),
 }));
 
