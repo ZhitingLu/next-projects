bd2bce979e319b80f73323ffa490026c15c4b5eb
Zhiting Lu <zhitinglu@outlook.com>
2025-06-10 18:03:34 +0200

    add subscription handling and modal integration for user authentication

--- x-next/src/app/components/Subscription.jsx
+++ x-next/src/app/components/Subscription.jsx
@@ -1,6 +1,24 @@
+"use client";
+
 import React from "react";
+import { useSession } from "next-auth/react";
+import useModalStore from "../stores/modalStore";
+import { toast } from "react-hot-toast";
 
 export default function Subscription() {
+  const { data: session } = useSession();
+  const { openModal } = useModalStore();
+
+  const handleSubscribtion = () => {
+    if (!session?.user) {
+      openModal("auth"); // opens Zustand auth modal
+    } else {
+      toast("Coming soon!", {
+        icon: "🥁",
+      });
+    }
+  };
+
   return (
     <div className="border border-gray-200 rounded-xl p-3">
       <div className="flex flex-col gap-2">
@@ -10,6 +28,7 @@ export default function Subscription() {
           revenue.
         </p>
         <button
+          onClick={handleSubscribtion}
           className="bg-[var(--twitter-blue)] text-white rounded-full 
 hover:brightness-95 transition-all duration-200 w-28 h-9
  cursor-pointer shadow-md hidden xl:inline font-semibold"
