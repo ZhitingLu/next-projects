6676dfacda145223fc1b4caea1567547c42e0a86
ZhitingLu <zhitinglu@outlook.com>
2025-06-11 14:59:12 +0200

    refactor Post component to enhance props handling and improve conditional rendering

--- x-next/src/app/components/Post.jsx
+++ x-next/src/app/components/Post.jsx
@@ -3,9 +3,16 @@ import { HiDotsHorizontal } from "react-icons/hi";
 import Link from "next/link";
 import Icons from "./Icons";
 
-export default function Post({ post, id, hideIcons = false, noBorder = false }) {
+export default function Post({
+  post,
+  id,
+  hideIcons = false,
+  commentStyle = false,
+  noLink = false,
+  hideImg = false
+}) {
   return (
-    <div className={`w-full flex p-3 ${noBorder ? '' : 'border-b border-gray-200'} hover:bg-gray-50`}>
+    <div className={`w-full flex p-3 ${commentStyle ? '' : 'border-b border-gray-200 hover:bg-gray-50'}`}>
       <img
         src={post?.profileImg}
         alt=""
@@ -21,19 +28,28 @@ export default function Post({ post, id, hideIcons = false, noBorder = false }) 
           <HiDotsHorizontal className="text-sm" />
         </div>
 
-        <Link href={`/posts/${id}`}>
+        {noLink ? (
           <p className="text-slate-800 text-sm my-3 whitespace-normal break-words">
             {post?.text}{" "}
           </p>
-        </Link>
+        ) :
+          (<Link href={`/posts/${id}`}>
+            <p className="text-slate-800 text-sm my-3 whitespace-normal break-words">
+              {post?.text}{" "}
+            </p>
+          </Link>
+          )}
 
-        <Link href={`/posts/${id}`}>
-          <img
-            src={post?.image}
-            alt=""
-            className="rounded-2xl mr-2 max-w-full h-auto"
-          />
-        </Link>
+        {!hideImg && (
+          <Link href={`/posts/${id}`}>
+            <img
+              src={post?.image}
+              alt=""
+              className="rounded-2xl mr-2 max-w-full h-auto"
+            />
+          </Link>
+        )}
+
         {!hideIcons && <Icons id={id} uid={post.uid} />}
       </div>
     </div>
