b9956a2c9851e0bcaa029a8cad293508004d9a87
ZhitingLu <zhitinglu@outlook.com>
2025-06-12 15:55:09 +0200

    fetch and display post data from Firestore

--- x-next/src/app/posts/[id]/page.js
+++ x-next/src/app/posts/[id]/page.js
@@ -1,7 +1,24 @@
-import React from 'react'
+// asynchronous 
+// server side
 
-export default function PostPage() {
+import React from 'react';
+import { app } from '../../../firebase';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+
+export default async function PostPage({ params }) {
+  const db = getFirestore(app);
+  let data = {};
+  const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+  data = querySnapshot.exists() ? { id: querySnapshot.id, ...querySnapshot.data() } : null;
+  if (!data) {
+    return <div>Post not found</div>;
+  }
+  // You can pass the data to a component or render it directly here
+  // For example, you can use a Post component to display the post
   return (
-    <div>PostPage</div>
+    <div>
+      {data.id}
+      <p>{data.text}</p>
+    </div>
   )
 }
