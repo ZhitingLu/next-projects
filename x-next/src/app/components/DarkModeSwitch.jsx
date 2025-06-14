c261da73cc2e65d5a9814b82a0223249681e7ec9
Zhiting Lu <zhitinglu@outlook.com>
2025-06-14 19:08:13 +0200

    improved accessibility

--- x-next/src/app/components/DarkModeSwitch.jsx
+++ x-next/src/app/components/DarkModeSwitch.jsx
@@ -17,15 +17,27 @@ export default function DarkModeSwitch() {
     <div>
       {mounted &&
         (currentTheme === "dark" ? (
-          <MdLightMode
-            className="w-6 h-6 cursor-pointer  text-xl hover:text-amber-500"
+          <div
+            className="flex items-center p-3 rounded-full transition-all duration-200 gap-6 w-fit
+    text-gray-800 hover:text-amber-500
+    dark:text-gray-200 dark:hover:text-text-amber-500
+    hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
             onClick={() => setTheme("light")}
-          />
+          >
+            <MdLightMode className="w-6 h-6  text-xl " />
+            <span className="text-xl hidden xl:inline">Switch</span>
+          </div>
         ) : (
-          <MdDarkMode
-            className="w-6 h-6 cursor-pointer text-xl hover:text-amber-500"
+          <div
+            className="flex items-center p-3 rounded-full transition-all duration-200 gap-6 w-fit
+  text-gray-800 hover:text-amber-500
+  dark:text-gray-200 dark:hover:text-text-amber-500
+  hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
             onClick={() => setTheme("dark")}
-          />
+          >
+            <MdDarkMode className="w-6 h-6 text-xl" />
+            <span className="text-xl hidden xl:inline">Switch</span>
+          </div>
         ))}
     </div>
   );
