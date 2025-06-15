9761f6f702b3f49ffcfc6746bf999ef9bbfeaa31
Zhiting Lu <zhitinglu@outlook.com>
2025-06-15 21:24:02 +0200

    render children only after mounting for improved theme handling

--- x-next/src/app/Providers.jsx
+++ x-next/src/app/Providers.jsx
@@ -1,18 +1,36 @@
-'use client';
+"use client";
 
 // The Providers is client side
 // but everything inside it is server side
 // Next.js directory is smart :)
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
 
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-export default function Providers({children}) {
+export default function Providers({ children }) {
+  const [mounted, setMounted] = useState(false);
+
+  // This effect ensures that the theme is only set after the component mounts
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <div className='text-gray-700 dark:text-gray-200 bg-white dark:bg-black select-none transition-colors duration-300'>
-        {children}
-      </div>
+      {/* Only render children after the component has mounted */}
+
+      {mounted ? (
+        <div className="text-gray-700 dark:text-gray-200 bg-white dark:bg-black select-none transition-colors duration-300">
+          {children}
+        </div>
+      ) : (
+        <div className="invisible">
+          {children}
+        </div>
+      )}
     </ThemeProvider>
-  )
+  );
 }
